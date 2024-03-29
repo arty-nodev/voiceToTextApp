09ea35938eb204ae4bc0626d7e90a9f658a2131a
arty-nodev <arttyvg@gmail.com>
2024-03-29 17:11:28 +0100

    se añade tailwind al proyecto

--- voicetotext-frontend/src/app/app.routes.ts
+++ voicetotext-frontend/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeVttComponent } from './components/home-vtt/home-vtt.component';
 
-export const routes: Routes = [{ path: 'home', component: HomeVttComponent }];
+export const routes: Routes = [
+  { path: 'home', component: HomeVttComponent },
+  { path: '**', component: HomeVttComponent },
+];
