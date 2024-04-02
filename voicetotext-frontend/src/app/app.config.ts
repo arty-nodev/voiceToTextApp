265a5c03ed07b47ad873d275cab8d60c42b5b067
arty-nodev <arttyvg@gmail.com>
2024-04-02 15:52:09 +0200

    se añade pulse lotties

--- voicetotext-frontend/src/app/app.config.ts
+++ voicetotext-frontend/src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideLottieOptions } from 'ngx-lottie';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideLottieOptions({ player: () => import('lottie-web') }),
+  ],
 };
--- voicetotext-frontend/src/app/components/home-vtt/home-vtt.component.ts
+++ voicetotext-frontend/src/app/components/home-vtt/home-vtt.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-home-vtt',
   standalone: true,
-  imports: [],
+  imports: [LottieComponent],
   templateUrl: './home-vtt.component.html',
-  styleUrl: './home-vtt.component.scss'
+  styleUrl: './home-vtt.component.scss',
 })
 export class HomeVttComponent {
-
+  public options: AnimationOptions = {
+    path: '../../../assets/lotties/anim-pulse.json',
+    
+  };
 }
